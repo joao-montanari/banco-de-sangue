2d24a4b72267acd34c1c427eb011dbb84c29d334
João Vitor Montanari <joao.unipbr@gmail.com>
2025-04-27 23:07:43 -0300

    :construction: feat: Adiciona um exemplar de gráfico na página incial

--- frontend/lib/Screens/home.dart
+++ frontend/lib/Screens/home.dart
@@ -1,4 +1,6 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+
 import 'package:frontend/Components/header.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -25,14 +27,47 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomHeader(title: 'Banco de sangue'),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 30.0, horizontal: 120),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'TORNE-SE UM DOADOR DE SANGUE',
+              style: TextStyle(
+                color: Color(0xffDF222B),
+                fontSize: 40,
+              ),
+            ),
+            Text(
+              'Todos os dias acontecem centenas de acidentes, cirurgias e queimaduras violentas que exigem transfusão, assim como os portadores de hemofilia, leucemia e anemias. Além disso, doar sangue é um ato simples, tranquilo e seguro que não provoca risco.',
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 50),
+              child: SizedBox(
+                // width: 300,
+                height: 300,
+                child: LineChart(
+                  LineChartData(
+                    lineBarsData: [
+                      LineChartBarData(
+                        spots: [
+                          FlSpot(0, 1),
+                          FlSpot(1, 3),
+                          FlSpot(2, 7),
+                          FlSpot(3, 4),
+                        ],
+                        isCurved: true,
+                        barWidth: 4,
+                        // belowBarData: BarAreaData(show: false),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ),
           ],
         ),
